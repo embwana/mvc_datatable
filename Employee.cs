4f2248166df94873645f796756e4fbeda6ceb0b3
embwana <36712217+embwana@users.noreply.github.com>
2019-06-01 15:31:58 +0200

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace DatatableCRUD.Models.Extended
+{  
+    [MetadataType(typeof(EmployeeMetadata))]
+    public partial class Employee
+    {
+
+    }
+    public class EmployeeMetadata
+    {
+        [Required (AllowEmptyStrings = false,ErrorMessage ="Please provide first name")]
+        public string Firstname { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please provide last name")]
+        public string Lirstname { get; set; }
+    }
+}
